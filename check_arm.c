b6356ee5d479fc73cfb84667a1a1ec372782f6be
Jim McBride <jim@signalmine.com>
2023-10-25 17:15:50 -0500

    check some errors, clean up after yourself

--- check_arm.c
+++ check_arm.c
@@ -4,56 +4,50 @@
 #include <grub/dl.h>
 #include <grub/command.h>
 
-/* This is a simple module to check if the CPU is ARM or not. 
+/* This is a simple module to check if the CPU is ARM or not.
  * It's not perfect, but it's good enough for our purposes.
  * -jm 10/25/2023 jim.mcbride@gmail.com
- * 
- * compile as a grub module.  insmod it, run check_arm and it will set the environment variable
- * cpu_type to arm if it's running on an ARM CPU.  Then you can access it in the grub environment
- * like this:
- * menuentry "Check CPU Architecture" {
- *   insmod echo
- *   echo "Checking CPU Architecture..."
- *   insmod check_arm
- *   check_arm
  *
- *   if [ "${cpu_type}" = "arm" ]; then
- *       echo "ARM CPU detected - Please insert logic to start AmiBerry here."
- *       sleep 10
- *       # Do arm stuff
- *   else
- *       echo "Doing not ARM stuff"
- *   fi
- *}
- */
+*/
 
 GRUB_MOD_LICENSE("GPLv3+");
 
-static grub_err_t grub_cmd_check_arm(grub_command_t cmd __attribute__ ((unused)),
-                                     int argc __attribute__ ((unused)),
-                                     char *argv[] __attribute__ ((unused)))
+static void check_arm(void)
 {
     grub_uint32_t value = 0;
 
-    // A basic MOV instruction
-    asm volatile("mov %0, #42" : "=r"(value));
+    // Print ASCII Art
+    grub_printf("   ,--.\n");
+    grub_printf(" /      \\\n");
+    grub_printf("<  πMiga  >\n");
+    grub_printf(" \\      /\n");
+    grub_printf("   `--'\n");
 
-    // If the value is 42, it's likely an ARM CPU
-    if (value == 42) {
+    // Execute a basic ARM MOV instruction to move the value 42 into a register.
+    asm volatile("mov %0, #42"
+                 : "=r"(value));
+
+    // Check the result to determine if we're on an ARM CPU.
+    if (value == 42)
+    {
         grub_env_set("cpu_type", "arm");
+        grub_printf("Running on ARM CPU. Set cpu_type=arm.\n");
     }
+    else
+    {
+        grub_printf("Not running a known ARM cpu.\n");
 
-    return GRUB_ERR_NONE;
+        // Set an error environment variable (optional)
+        grub_env_set("cpu_error", "unknown_cpu_type");
+    }
 }
-
-static grub_command_t cmd;
 
 GRUB_MOD_INIT(check_arm)
 {
-    cmd = grub_register_command("check_arm", grub_cmd_check_arm, 0, "Checks if running on ARM and sets an environment variable.");
+    check_arm();
 }
 
 GRUB_MOD_FINI(check_arm)
 {
-    grub_unregister_command(cmd);
+    // Cleanup actions, if needed.
 }
